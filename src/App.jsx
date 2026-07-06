1f6da658d0f9770d832619aa37929262c012fe24
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-07-06 19:33:25 +0300

    update the layout to make it available the header to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Header from "./components/Header/Header"
 import './App.css'
 import Landing from "./Pages/Landing/Landing"
 import Router from "./Pages/Router"
+import LayOut from "./components/LayOut/LayOut"
 
 function App() {
 
   return (
-    <>
+    <LayOut>
     <Router/>
-    </>
+    </LayOut>
   )
 }
 
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import CarouselEffect from "../../components/CarouselEffect/CarouselEffect";
 import CategoryGrid from "../../components/CategoryGrid/CategoryGrid";
 import Products from "../../components/Products/Products";
-import LayOut from "../../components/LayOut/LayOut";
 
 function Landing() {
   return (
-    <LayOut>
+    <>
       <CarouselEffect />
       <CategoryGrid />
       <Products />
-    </LayOut>
+    </>
   );
 }
 
