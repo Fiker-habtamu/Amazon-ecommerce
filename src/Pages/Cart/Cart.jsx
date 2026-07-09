934f7a226e6d1dfd786afca9d2cebda752361daa
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-07-09 13:28:56 +0300

    cart style edit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -9,7 +9,6 @@ function Cart() {
   let total = basket.reduce((amount,item)=>{
 	return  amount + item.price * item.amount
   },0)
-  console.log(basket)
   return (
     <div className="cart-page-container">
       {/* Left Column: Basket Summary Listing */}
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -1,26 +1,34 @@
 import { Type } from "./action.type";
 
 let initialState = {
-	basket:[]
-}
+  basket: [],
+};
 
-let reducer=(state,action)=>{
-	switch (action.type) {
-		case Type.ADD_TO_CART:
-			let existingItem = state.basket.find((item)=>{
-				return item.id === action.item.id
-			})
-			if(!existingItem){
-				return {...state,basket:[...state.basket,{...action.item,amount:1}]}
-			}else{
-				let updateExistingItem = state.basket.map((item)=>{
-					return item.id == action.item.id? {...item,amount:item.amount + 1}:item
-				})
-				return {...state,basket:updateExistingItem}
-			}
-		default:
-			return state
-	}
-}
+let reducer = (state, action) => {
+  switch (action.type) {
+    case Type.ADD_TO_CART:
+      let existingItem = state.basket.find((item) => {
+        return item.id === action.item.id;
+      });
+      if (!existingItem) {
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        let updateExistingItem = state.basket.map((item) => {
+          return item.id == action.item.id
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
+        return {
+          ...state,
+          basket: updateExistingItem,
+        };
+      }
+    default:
+      return state;
+  }
+};
 
-export {initialState,reducer};
+export { initialState, reducer };
